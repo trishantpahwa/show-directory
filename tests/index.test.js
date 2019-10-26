0a5f63ccd052ae2b0dc5240c4587897ac77b6e4b
Trishant Pahwa <trishantpahwa@gmail.com>
2019-10-26 13:49:43 +0530

    Trying to fix bug in tests, attempt-2.

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,12 +5,11 @@ import showDirectory from './index';
 
 function Testing_Directory_Contents() {
     describe('Testing sample_folder', function() {
-        console.log(__dirname);
-        var path = __dirname.toString().split('\\');
-        console.log(path);
+        var path = __dirname.toString()
         path.pop();
+        console.log(path);
         path = path.join('\\') + '\\dist\\sample_folder';
-
+        console.log(path);
         describe('Directory Path' + path, function() {
             it('should have the same directory.', function() {                
                 fs.readFile('dist\/sample_folder_test_output.json', function(err, data) {
