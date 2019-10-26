ccc15311f5a3f8b692a4c9c56be4efca6c689f43
Trishant Pahwa <trishantpahwa@gmail.com>
2019-10-26 13:36:56 +0530

    Updated tests.

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,24 +3,11 @@ import assert from 'assert';
 
 import showDirectory from './index';
 
-function Test_Directory_Path() {
-    describe('Testing directory path', function() {
-        var path = __dirname.toString().split('\\');
-        path.pop();
-        path = path.join('\\') + '\\sample_folder';
-        it('should have the same path.', function() {
-            console.log('Input path: ' + path);
-            console.log('Current directory: ' + __dirname);
-            assert.equal(path, __dirname);
-        });
-    });
-}
-
 function Testing_Directory_Contents() {
     describe('Testing sample_folder', function() {
         var path = __dirname.toString().split('\\');
         path.pop();
-        path = path.join('\\') + '\\sample_folder';
+        path = path.join('\\') + '\\dist\\sample_folder';
         describe('Directory Path' + path, function() {
             it('should have the same directory.', function() {                
                 fs.readFile('dist\/sample_folder_test_output.json', function(err, data) {
@@ -33,5 +20,4 @@ function Testing_Directory_Contents() {
     });
 }
 
-Test_Directory_Path();
 Testing_Directory_Contents();
